009b1c03857b87882c62801a66a7aece239926ba
Chad Milando <44262671+cmilando@users.noreply.github.com>
2024-06-21 16:12:55 -0400

    updates

--- 02_make_plots.R
+++ 02_make_plots.R
@@ -16,7 +16,7 @@ ggplot() +
 ggplot() +
   geom_sf(data = state_map_sf, fill = "white", color = "black") +
   geom_sf(data = population_points_sf, color = "forestgreen", size = 0.5) +
-  geom_sf(data = highway_lines_sf, color = "orange", size = 0.1, alpha = 0.5) +
+  geom_sf(data = highway_lines_sf, color = "grey", size = 0.1, alpha = 0.5) +
   #geom_sf(data = filtered_points, color = "red", size = 0.5) +
   theme_minimal() +
   labs(title = "Population Points and Major Highways in Selected Northeastern States")
